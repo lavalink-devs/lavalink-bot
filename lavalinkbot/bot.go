60ade5207e4f7231f2e5edc1d4a1551f50ccb176
topi314 <git@topi.wtf>
2023-11-22 12:11:59 +0100

    make bot fail to start when no node connects

--- lavalinkbot/bot.go
+++ lavalinkbot/bot.go
@@ -2,12 +2,15 @@ package lavalinkbot
 
 import (
 	"context"
+	"os"
+	"sync"
 	"time"
 
 	"github.com/disgoorg/disgo/bot"
 	"github.com/disgoorg/disgolink/v3/disgolink"
 	"github.com/disgoorg/log"
 	"github.com/google/go-github/v52/github"
+
 	"github.com/lavalink-devs/lavalink-bot/internal/maven"
 )
 
@@ -26,12 +29,24 @@ func (b *Bot) Start() error {
 	if err := b.Client.OpenGateway(ctx); err != nil {
 		return err
 	}
-	for _, node := range b.Cfg.Nodes {
-		if _, err := b.Lavalink.AddNode(ctx, node.ToNodeConfig()); err != nil {
-			log.Errorf("failed to add lavalink node %s: %s", node.Name, err)
-		} else {
-			log.Infof("added lavalink node: %s", node.Name)
-		}
+
+	var wg sync.WaitGroup
+	for i := range b.Cfg.Nodes {
+		wg.Add(1)
+		go func(node NodeConfig) {
+			defer wg.Done()
+			if _, err := b.Lavalink.AddNode(ctx, node.ToNodeConfig()); err != nil {
+				log.Errorf("failed to add lavalink node %s: %s", node.Name, err)
+			} else {
+				log.Infof("added lavalink node: %s", node.Name)
+			}
+		}(b.Cfg.Nodes[i])
+	}
+
+	wg.Wait()
+	if node := b.Lavalink.BestNode(); node == nil {
+		log.Error("no node connected")
+		os.Exit(-1)
 	}
 
 	return nil
